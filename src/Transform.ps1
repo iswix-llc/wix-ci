7ceb13cb8d722d5407c99cd519c1b463d4c32d52
Christopher Painter <chrpai@iswix.com>
2025-03-24 22:31:50 -0500

    Update Transform.ps1

--- src/Transform.ps1
+++ src/Transform.ps1
@@ -1,4 +1,13 @@
 $ErrorActionPreference = "Stop"
 Rename-Item -Path .\wix\WixToolset.Sdk -NewName IsWiXToolset.Sdk
 Rename-Item -Path .\wix\IsWixToolset.Sdk\WixToolset.Sdk.csproj -NewName IsWixToolset.Sdk.csproj
+Rename-Item -Path .\wix\IsWixToolset.Sdk\WixToolset.Sdk.nuspec -NewName IsWixToolset.Sdk.nuspec
+
+# Update Solution
 (Get-Content .\wix\wix.sln) -replace "WiXToolset.Sdk", "IsWiXToolSet.Sdk" | Set-Content .\wix\wix.sln
+
+#Rebrand SDK
+(Get-Content .\wix\IsWixToolset.Sdk\IsWixToolset.Sdk.nuspec) -replace 'type="file"', 'type="expression' | Set-Content .\wix\IsWixToolset.Sdk\IsWixToolset.Sdk.nuspec
+(Get-Content .\wix\IsWixToolset.Sdk\IsWixToolset.Sdk.nuspec) -replace 'type="OSMFEULA.txt"', 'MS-RL' | Set-Content .\wix\IsWixToolset.Sdk\IsWixToolset.Sdk.nuspec
+
+
