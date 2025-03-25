9aaa468b86edc63e750f3c715bea8f8984a454f6
Christopher Painter <chrpai@iswix.com>
2025-03-24 22:05:35 -0500

    test

--- src/Transform.ps1
+++ src/Transform.ps1
@@ -1,1 +1,4 @@
-Write-Host Hello
+$ErrorActionPreference = "Stop"
+Rename-Item -Path .\wix\WixToolset.Sdk -NewName IsWiXToolset.Sdk
+Rename-Item -Path .\wix\IsWixToolset.Sdk\WixToolset.Sdk.csproj -NewName IsWixToolset.Sdk.csproj
+(Get-Content .\wix\wix.sln) -replace "WiXToolset.Sdk", "IsWiXToolSet.Sdk" | Set-Content .\wix\wix.sln
