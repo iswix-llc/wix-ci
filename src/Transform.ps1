1b5af658bb5577bf97e2634f79d0d52fd8c8af62
Christopher Painter <chrpai@iswix.com>
2025-03-24 22:48:54 -0500

    Update Transform.ps1

--- src/Transform.ps1
+++ src/Transform.ps1
@@ -1,13 +1,20 @@
 $ErrorActionPreference = "Stop"
-Rename-Item -Path .\wix\WixToolset.Sdk -NewName IsWiXToolset.Sdk
-Rename-Item -Path .\wix\IsWixToolset.Sdk\WixToolset.Sdk.csproj -NewName IsWixToolset.Sdk.csproj
-Rename-Item -Path .\wix\IsWixToolset.Sdk\WixToolset.Sdk.nuspec -NewName IsWixToolset.Sdk.nuspec
 
 # Update Solution
 (Get-Content .\wix\wix.sln) -replace "WiXToolset.Sdk", "IsWiXToolSet.Sdk" | Set-Content .\wix\wix.sln
 
 #Rebrand SDK
-(Get-Content .\wix\IsWixToolset.Sdk\IsWixToolset.Sdk.nuspec) -replace 'type="file"', 'type="expression' | Set-Content .\wix\IsWixToolset.Sdk\IsWixToolset.Sdk.nuspec
-(Get-Content .\wix\IsWixToolset.Sdk\IsWixToolset.Sdk.nuspec) -replace 'type="OSMFEULA.txt"', 'MS-RL' | Set-Content .\wix\IsWixToolset.Sdk\IsWixToolset.Sdk.nuspec
+Rename-Item -Path .\wix\WixToolset.Sdk -NewName IsWiXToolset.Sdk
+Rename-Item -Path .\wix\IsWixToolset.Sdk\WixToolset.Sdk.csproj -NewName IsWixToolset.Sdk.csproj
+Rename-Item -Path .\wix\IsWixToolset.Sdk\WixToolset.Sdk.nuspec -NewName IsWixToolset.Sdk.nuspec
 
+
+#Update All Nuspec Files
+Get-ChildItem -recurse -Include *.nuspec |
+Foreach-Object {
+    $content = Get-Content $_.FullName
+
+(Get-Content $_.FullName) -replace 'type="file"', 'type="expression"' | Set-Content $_.FullName
+(Get-Content $_.FullName) -replace 'OSMFEULA.txt', 'MS-RL' | Set-Content $_.FullName
+}
 
